dca524ee85c4a341e1f206137901e18745c40bce
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-03-08 21:58:21 +0100

    example based on OTL 2.6

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from pathlib import Path
+
+from rdflib import Graph, Namespace, URIRef, RDF, RDFS, OWL, Literal, SH, BNode, SKOS
+from pyshacl import validate
+
+from OTLShaclGenerator import OTLShaclGenerator
+
+
+if __name__ == '__main__':
+    subset_path = Path('OTL2.6.db')
+    shacl_path = Path('generated_shacl_otl_2.6.ttl')
+    ont_path = Path('generated_ont_otl_2.6.ttl')
+    shacl, ont = OTLShaclGenerator.generate_shacl_from_otl(subset_path=subset_path, shacl_path=shacl_path,
+                                                           ont_path=ont_path)
